ddefe9efb9ee45683e4a765e32a26094c44c8a37
rajmohan <rajmohan@techaffinity.com>
2021-12-19 23:17:07 +0530

    refact

--- src/App.js
+++ src/App.js
@@ -10,28 +10,31 @@ function App() {
               <img src="/logoo.png" alt="" />
             </div>
             <div className="">
-              <ul className="flex items-center space-x-16 ">
+              <ul className="flex items-center space-x-8 ">
                 <li>
-                  <div >Home</div>
+                  <div>Home</div>
                 </li>
                 <li>
-                  <div >About</div>
+                  <div>About</div>
                 </li>
                 <li>
-                  <div >Block</div>
+                  <div>Core Services</div>
                 </li>
                 <li>
-                  <div >Contact</div>
+                  <div>World of Computation</div>
                 </li>
                 <li>
-                  <div >Help</div>
+                  <div>Our latest Project</div>
+                </li>
+                <li>
+                  <div>Help</div>
                 </li>
               </ul>
             </div>
             <div className="space-x-5 text-black">
-              <button className="w-24 py-2 bg-white rounded-lg">Login</button>
-              <button className="w-24 py-2 rounded-lg bg-l-green">
-                Register
+              {/* <button className="w-24 py-2 bg-white rounded-lg">Login</button> */}
+              <button className="px-8 py-2 rounded-lg bg-l-green">
+                Contact Us
               </button>
             </div>
           </div>
@@ -40,15 +43,14 @@ function App() {
 
         <div className="grid grid-cols-2 py-20 place-items-center">
           <div className="space-y-10 ">
-            <div className="text-3xl text-l-green">Block portal</div>
+            <div className="text-3xl text-l-green">AI-powered</div>
 
             <div className="font-black text-white text-8xl">
-              Powerfull <br></br> Crypto App
+              solutions <br></br> provider
             </div>
 
             <div className="text-lg text-white">
-              Let’s explore the world of Artificial Intelligence Let’s explore
-              the world of Artificial Intelligence{" "}
+              Let’s explore the world of Artificial Intelligence.
             </div>
 
             <div className="">
@@ -66,18 +68,49 @@ function App() {
           </div>
         </div>
         {/*  */}
-
+        <div className="text-center text-white">
+          <div className="max-w-4xl py-10 mx-auto space-y-8">
+            <div className="text-6xl font-bold text-l-green "> About</div>
+            <div className="text-white text-opacity-60 ">
+              Flystrix Technologies was founded by a team of Aerospace engineers
+              while participating in a contest conducted by the Indian Airforce
+              for the development of swarm drone technology. The team progressed
+              into advanced stages of the contest which involved design and
+              development of AI based Computer vision algorithms that can detect
+              and categorise human/wildlife, and recognize the human hand
+              signals and infer its meaning. Since then, Flystrix Technologies
+              has evolved into a AI Powered solutions provider with key
+              expertise in Machine Learning, Neural Networks, Video Analytics,
+              Image Analysis, Predictive Analysis, AI powered product
+              development - from scratch & enhancements as well. We are
+              proficient in solving problems intuitively. Our team consists of
+              experts from Electronics, Avionics, Machine learning, Database
+              Management, and Aerospace engineering.
+            </div>
+          </div>
+        </div>
         <div className="text-center text-white">
           <div className="py-10">
-            <div className="text-xl text-l-green">We serve You</div>
-            <div className="text-4xl font-bold ">Best features</div>
+            <div className="text-4xl font-bold text-l-green ">
+              Core Services
+            </div>
+            <div className="text-xl text-white">Best features </div>
           </div>
-          <div className="grid grid-cols-3 text-lg place-content-center place-items-center ">
+          <div className="grid grid-cols-3 gap-12 text-lg place-content-center place-items-center ">
             <div className="space-y-3 ">
               <div className="">
                 <img className="mx-auto" src="/img1.png" alt="" />
               </div>
-              <div className="text-2xl font-bold">Secure</div>
+              <div className="text-2xl font-bold">Machine Learning</div>
+              <div className="mx-auto text-white text-opacity-60 w-60">
+                We Provide the best Security For Your account
+              </div>
+            </div>
+            <div className="space-y-3 ">
+              <div className="">
+                <img className="mx-auto" src="/img1.png" alt="" />
+              </div>
+              <div className="text-2xl font-bold">Predictive Analysis</div>
               <div className="mx-auto text-white text-opacity-60 w-60">
                 We Provide the best Security For Your account
               </div>
@@ -86,7 +119,9 @@ function App() {
               <div className="">
                 <img className="mx-auto" src="/img2.png" alt="" />
               </div>
-              <div className="text-2xl font-bold">Detailed Report</div>
+              <div className="text-2xl font-bold">
+                AI powered Product Diagnostics
+              </div>
               <div className="mx-auto text-white text-opacity-60 w-60">
                 Detailed transaction and financial reports
               </div>
@@ -95,7 +130,16 @@ function App() {
               <div className="">
                 <img className="mx-auto" src="/img3.png" alt="" />
               </div>
-              <div className="text-2xl font-bold">Smart Data Entry</div>
+              <div className="text-2xl font-bold">Image Analysis</div>
+              <div className="mx-auto text-white text-opacity-60 w-60">
+                very High Accuracy and Speed for data entry
+              </div>
+            </div>
+            <div className="space-y-3 ">
+              <div className="">
+                <img className="mx-auto" src="/img3.png" alt="" />
+              </div>
+              <div className="text-2xl font-bold">Video Analysis</div>
               <div className="mx-auto text-white text-opacity-60 w-60">
                 very High Accuracy and Speed for data entry
               </div>
@@ -104,26 +148,29 @@ function App() {
         </div>
         {/*  */}
 
-        <div className="grid grid-cols-2 my-20 place-items-center">
+        <div className="grid grid-cols-2 p-8 my-20 bg-black place-items-center">
           <div className="space-y-10 text-white">
-            <div className="text-3xl text-l-green">Block portal App</div>
-            <div className="text-5xl font-bold ">
-              Real-Time Update and Monitoring your Crypto
-            </div>
+            <div className="text-3xl text-l-green">AI-powered</div>
+            <div className="text-5xl font-bold ">World of computing vision</div>
             <div className="text-white text-opacity-60">
-              A collection of Node JS and Golang Backend interview questions
-              please feel free to fork and contribute to this repository A
-              collection of Node JS and Golang Backend interview questions
-              please feel free to fork and contribute to this repository A
-              collection of Node JS and Golang Backend interview questions
-              please feel free to fork and contribute to this repository A
-              collection of Node JS and Golang Backend interview questions
-              please feel free to fork and contribute to this repository A
-              collection of Node JS and Golang Backend interview questions
-              please feel free to fork and contribute to this repository A
-              collection of Node JS and Golang Backend interview questions
-              please feel free to fork and contribute to this repository
+              <ul className="ml-8 list-disc">
+                <li> Posture Detection</li>
+                <li>Medical Image Analysis</li>
+                <li>Automated Management System</li>
+                <li>Face recognition</li>
+                <li>ALPR/ Toll Gate Applications</li>
+
+                <li>Rover</li>
+                <li>Product Identification</li>
+                <li>Object Tracking</li>
+                <li>Damage Identification</li>
+              </ul>
             </div>
+            <p>
+              The potential applications of computer vision are endless. Few
+              basic scenarios where our computer vision technology can step in
+              to fulfil industrial needs:
+            </p>
             <div className="">
               <button className="px-12 py-3 text-xl text-black rounded-lg bg-l-green">
                 Get Started
@@ -132,13 +179,50 @@ function App() {
           </div>
           <div className="">
             <div className="">
-              <img className="max-w-sm" src="/mob.png" alt="" />
+              <img className="rounded-b-full " src="/brain.jpg" alt="" />
             </div>
           </div>
         </div>
 
         {/*  */}
-
+        <div className="pb-40 text-center text-white">
+          <div className="py-10">
+            <div className="text-4xl font-bold text-l-green ">
+              Our latest Project
+            </div>
+          </div>
+          <div className="grid grid-cols-3 gap-12 text-lg place-content-center place-items-center ">
+            <div className="space-y-3 ">
+              <div className="">
+                <img className="mx-auto rounded-full" src="/img1.png" alt="" />
+              </div>
+              <div className="text-2xl font-bold">Machine Learning</div>
+              <div className="mx-auto text-white text-opacity-60 w-60">
+                We Provide the best Security For Your account
+              </div>
+            </div>
+            <div className="space-y-3 ">
+              <div className="">
+                <img className="mx-auto rounded-full" src="/img1.png" alt="" />
+              </div>
+              <div className="text-2xl font-bold">Predictive Analysis</div>
+              <div className="mx-auto text-white text-opacity-60 w-60">
+                We Provide the best Security For Your account
+              </div>
+            </div>
+            <div className="space-y-3 ">
+              <div className="">
+                <img className="mx-auto rounded-full" src="/img2.png" alt="" />
+              </div>
+              <div className="text-2xl font-bold">
+                AI powered Product Diagnostics
+              </div>
+              <div className="mx-auto text-white text-opacity-60 w-60">
+                Detailed transaction and financial reports
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="pb-12">
           <div className="text-l-green">Testimonal</div>
 
@@ -192,7 +276,7 @@ function App() {
                 <div className="p-12 space-y-4 box-bg-t rounded-2xl">
                   <div className="">
                     <img
-                      className="mx-auto rounded-full"
+                      className="w-20 h-20 mx-auto rounded-full"
                       src="/t1.png"
                       alt=""
                     />
@@ -214,16 +298,49 @@ function App() {
         </div>
         {/*  */}
 
-        <div className="grid grid-cols-3 p-16 my-16 mt-60 place-items-center rounded-3xl box-bg">
+        <div className="grid grid-cols-3 p-16 my-16 mt-60 place-items-start rounded-3xl box-bg">
           <div className="w-full col-span-2 space-y-12 ">
             <div className="">
-              <div className="text-6xl font-bold text-white">
-                Real-Time and Moni mr-16 ng your Crypt
+              <div className="text-5xl font-bold text-white">
+                Design Philosophy
               </div>
             </div>
-            <div className="text-xl text-white">
-              pdate and Monitoring your Crypt A collection of Node JS and Golang
-              Backend interview questions please feel free to{" "}
+            <div className="text-lg text-white">
+              <ul className="list-disc list-outside">
+                <li>To reduce the number of traffic accidents and deaths</li>
+                <li>
+                  To achieve a contactless enforcement system for rules outlined
+                  in MV Act.
+                </li>
+                <li>
+                  To improve efficiency in enforcing MV Act with minimal
+                  workforce.
+                </li>
+                <li> To minimize the workload of police personnel</li>
+                <li>To improve transparency.</li>
+                <li>
+                  To digitize and fast track prosecution of motor ‘petty’ cases
+                </li>
+                <li> To avoid police-public conflicts</li>
+                <li>
+                  To create awareness about the importance of adherence to
+                  guidelines outlined in MV Act
+                </li>
+                <li>
+                  To eliminate traffic congestion that may arise due to manual
+                  policing.
+                </li>
+                <li>To limit the paperwork involved.</li>
+                <li>To make roads safer for everyone.</li>
+                <li>
+                  To create a self-sustainable framework that ensures seamless
+                  enforcement of the MV Act.
+                </li>
+                <li>
+                  To become a frontrunner in adaptation of newer technologies.
+                </li>
+                <li>To generate more revenue for the government.</li>
+              </ul>
             </div>
             <div className="flex items-center space-x-6">
               <button className="flex items-center px-4 py-1 bgreen rounded-xl">
@@ -251,7 +368,7 @@ function App() {
           <div className="col-span-1">
             <img
               className="mx-auto -mt-60 "
-              src="/imgmob-removebg-preview.png"
+              src="/ai-removebg-preview (1).png"
               alt=""
             />
           </div>
